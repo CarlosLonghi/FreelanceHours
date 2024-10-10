19b22c903d932be65946289f2d27fe8836d21e9d
CarlosLonghi <carloslonghi.cl@gmail.com>
2024-10-10 11:55:28 -0300

    feat: add Proposal model/migrations/seeder/factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Proposal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,8 +21,12 @@ class DatabaseSeeder extends Seeder
 
         User::query()->inRandomOrder()->limit(10)->get()
             ->each(function (User $user) {
-                Project::factory()->create([
+                $project = Project::factory()->create([
                     'created_by' => $user,
+                ]);
+
+                Proposal::factory()->count(random_int(5, 30))->create([
+                    'project_id' => $project->id,
                 ]);
             });
     }
