6db004d8817816721fbb7003f3b53a396d28cc0b
CarlosLonghi <carloslonghi.cl@gmail.com>
2024-10-13 13:43:06 -0300

    fix: add @vite on app.blade

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,8 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{{ $title ?? 'Page Title' }}</title>
-        {{-- When production remove this comment --}}
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js'])  --}}
+        @vite(['resources/css/app.css', 'resources/js/app.js']) 
     </head>
 
     <body class="bg-[#070715] flex flex-col min-h-full text-[#C3C3D1]">
