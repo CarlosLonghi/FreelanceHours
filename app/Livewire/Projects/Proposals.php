e832e735cb44d9598b77815041f1af94bec8e680
CarlosLonghi <carloslonghi.cl@gmail.com>
2024-10-16 23:05:39 -0300

    feat: add loadMore proposals in page projects show

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -3,12 +3,37 @@
 namespace App\Livewire\Projects;
 
 use App\Models\Project;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Proposals extends Component
 {
     public Project $project;
 
+    public int $quantity = 6;
+    #[Computed()]
+    public function proposals()
+    {
+        return $this->project->proposals()
+            ->orderBy('hours', 'asc')
+            ->paginate($this->quantity);
+    }
+
+    #[Computed()]
+    public function lastProposalTime()
+    {
+        return $this->project->proposals()
+            ->latest()->first()
+            ->created_at->diffForHumans();
+    }
+
+    public function loadMore()
+    {
+        $this->quantity += 6;
+    }
+
+    #[On('proposal::created')]
     public function render()
     {
         return view('livewire.projects.proposals');
--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -33,6 +33,7 @@ class Create extends Component
             ['hours' => $this->hours]
         );
 
+        $this->dispatch('proposal::created');
         $this->modal = false;
     }
 }
